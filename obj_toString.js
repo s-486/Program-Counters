e194015eff349260575e0b3d5c2e2a3921d066fd
skb-9812 <116441392+skb-9812@users.noreply.github.com>
2022-10-23 22:47:41 +0530

    Create obj_toString.js

---
+++ obj_toString.js
@@ -0,0 +1,20 @@
+// program to convert an object to a string
+
+const person = {
+    name: 'Jack',
+    age: 27
+}
+
+const result1 = String(person);
+const result2 = String(person['name']);
+
+console.log(result1);
+console.log(result2);
+
+console.log(typeof result1);
+
+//Output:
+
+// [object Object]
+// Jack
+// string
